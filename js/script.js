54abea15390bad226ed5bfd8149d13a2f07f257a
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-03-01 15:47:44 +0600

    added js count and disabled functios

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@ document.getElementById('middleBox').addEventListener("click", function(){
     window.location.href = 'blogs.html'
 })
 
+
+
+const buttons = document.querySelectorAll(".completedBtn");
+for(const button of buttons ){
+    button.addEventListener('click', function(){
+        alert("Click OK");
+
+        // checkbox counting 
+        const count1 = parseInt(document.getElementById('count1').innerText);
+        const count2 = parseInt(document.getElementById('count2').innerText);
+        document.getElementById('count1').innerText = Math.max(0, count1 + 1);
+        document.getElementById('count2').innerText =Math.max(0, count2 - 1);
+
+        // disable button 
+        this.disabled = true;
+        
+    })
+}
+
+
+
