e7e03d742aa906335614a0af5b7cbe8634525820
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-03-01 17:45:24 +0600

    added date and time funtioonality

--- js/script.js
+++ js/script.js
@@ -18,19 +18,38 @@ for(const button of buttons ){
         // disable button 
         this.disabled = true;
 
+
+        // current date and time 
+        const time = new Date();
+        const currentTime= time.toLocaleTimeString('en-US', 
+          {
+              hour: 'numeric',
+              minute: 'numeric',
+              second : 'numeric',
+              hour12 : true 
+          })
+        const currentDate= time.toLocaleTimeString('en-US', 
+          {
+              day: 'numeric',
+              month : 'long',
+              year : 'numeric'
+          })
         // History Container
         const historyContainer = document.getElementById('historyContainer');
          const card = this.closest('.card-body');
          const title = card.querySelector(".card-title").innerText;
        const history = document.createElement('p');
        history.innerHTML= `
-        You Have Compeleted the ${title}  at 
+        You Have Compeleted the ${title} at ${currentTime}
        `
        history.style.backgroundColor = '#F4F7FF'
        history.style.padding = '16px'
        historyContainer.appendChild(history);
     
-    
+ 
+
+  
+
     })
 }
 
